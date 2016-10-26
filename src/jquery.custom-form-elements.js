9609db484d149aa5396720f3764ab59af118fa82
Andrew Ferri <andrew@vo2.co.nz>
2016-10-27 11:20:11 +1300

    Custom dropdown for select elements

--- src/jquery.custom-form-elements.js
+++ src/jquery.custom-form-elements.js
@@ -1,40 +1,79 @@
 ;(function($){
 
-  $.fn.customFormElement = function(options){
+    $.fn.customFormElement = function(options){
 
-    var settings = $.extend({
-      className: 'js-custom'
-    }, options);
+        var settings = $.extend({
+            className: 'js-custom'
+        }, options);
 
-    this.each(function(){
+        this.each(function(){
 
-      var $el = $(this);
+        var $el = $(this);
 
-      switch($el.prop('tagName')){
+        switch($el.prop('tagName')){
 
-        case 'INPUT':
-        $el.wrap('<span class="' + settings.className + '-' + $el.prop('type') + '" />');
-          $('<label for="' + $el.prop('id') + '" />').appendTo($el.parent());
-          break;
+            case 'INPUT':
+                $el.wrap('<span class="' + settings.className + '-' + $el.prop('type') + '" />');
+                $('<label for="' + $el.prop('id') + '" />').appendTo($el.parent());
+                break;
 
-        case 'SELECT':
-          $el.wrap('<span class="' + settings.className + '-select" />');
-          var $parent = $el.parent();
-          var options = $el.children();
-          var label = $('<span class="js-label" />')
-            .html($(options[$el.prop('selectedIndex')]).html())
-            .appendTo($parent);
+            case 'SELECT':
+                $el.wrap('<span class="' + settings.className + '-select" />');
+                var $parent = $el.parent();
+                var options = $el.children();
 
-          $el.on('change', function(){
+                var label = $('<span class="js-label" />')
+                    .html($(options[$el.prop('selectedIndex')]).html())
+                    .appendTo($parent);
 
-            var options = $el.children();
-            label.html($(options[$el.prop('selectedIndex')]).html());
-          });
-          break;
-      }
-    });
+                var list = $('<ul class="options" />').appendTo($parent);
 
-    return this;
-  };
+                $(options).each(function(index){
+
+                    var li = $('<li class="option">' + $(this).html() + '</li>').appendTo(list);
+                    if (index == $el.prop('selectedIndex')){
+
+                        $(li).addClass('selected');
+                    }
+                });
+
+                $(label).click(function(){
+
+                    $parent.toggleClass('open');
+                });
+
+                $(document.body).click(function(){
+
+                    $parent.removeClass('open');
+                });
+
+                $parent.click(function(e){
+
+                    e.stopPropagation();
+                });
+
+                $(list).children().each(function(index){
+
+                    $(this).click(function(){
+
+                        $(list).find('.selected').removeClass('selected');
+                        $(this).addClass('selected');
+                        $el.val($(options[index]).val());
+                        label.html($(options[index]).html());
+                        $parent.removeClass('open');
+                    });
+                });
+
+                $el.on('change', function(){
+
+                    var options = $el.children();
+                    label.html($(options[$el.prop('selectedIndex')]).html());
+                });
+                break;
+            }
+        });
+
+        return this;
+    };
 
 })(jQuery);
